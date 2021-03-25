3e382eda256c47c9974b141fd773972448f6ee3d
nicolasguidojoffre <ni.joffre@gmail.com>
2021-03-25 19:37:15 -0300

    agregandocondicionales

---
+++ script.js
@@ -0,0 +1,7 @@
+var estado = prompt ("Confirme aquí su estado civil");
+if(estado == "soltero" || estado == "soltero"){
+alert("Tu estado civil es " + estado + ". Puede pasar");
+}
+else {
+alert("Ponete las pilas")
+}
